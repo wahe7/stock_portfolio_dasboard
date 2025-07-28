27a4249e468b10bc2babe48fce1f38530b3ab30f
wahe7 <waheguru002200@gmail.com>
2025-07-29 01:46:42 +0530

    route fix

--- backend/routes/portfolio.js
+++ backend/routes/portfolio.js
@@ -2,7 +2,7 @@ const express = require("express")
 const router = express.Router()
 const {addPortfolio,getPortfolio,deleteStock,editStock} = require("../controller/portfolio")
 
-router.post("/addPortfolio",addPortfolio)
+router.post("/portfolio/addPortfolio",addPortfolio)
 router.get("/portfolio/:userId",getPortfolio)
 router.delete("/portfolio/:userId/:stockId",deleteStock)
 router.put("/portfolio/:userId/:stockId",editStock)
